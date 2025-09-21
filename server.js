a947c81bb556d5c08c98790cec69bc4d7926a0c2
gamepartys <civenmelih@gmail.com>
2025-09-21 12:30:41 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
-﻿const express = require("express");
+const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 /** rooms[CODE] yapısı:
  *  {
